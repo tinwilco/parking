46ee3d6f0d945ee7e13db362d842352535196aa3
martin.wilcock <martin.wilcock@raytheon.co.uk>
2020-11-03 17:26:05 +0000

    update origin point wildcard

--- handler.js
+++ handler.js
@@ -41,7 +41,7 @@ var result = await dynamo.query(params).promise()
         statusCode: responseCode,
         headers: {
             "Access-Control-Allow-Headers" : "Content-Type",
-            "Access-Control-Allow-Origin": "https://resttesttest.com",
+            "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
         },
         body: JSON.stringify(responseBody)
